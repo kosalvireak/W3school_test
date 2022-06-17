8018de9ad3ecc1836e07a9fc5ac6fc6533deb6c5
Kosalvireak Khoeun <virakvary@gmail.com>
2022-06-17 14:38:35 +0700

    Add project files.

---
+++ W3school_test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3school_test
+{
+    internal class Program
+    {
+		class Car
+		{
+			string strName = "vireak";
+			int intAge = 19;
+			static void Main(string[] args)
+			{
+				Car myObj = new Car();
+				Console.WriteLine(myObj.strName);
+				Console.WriteLine(myObj.intAge);
+			}
+		}
+
+
+	}
+}
